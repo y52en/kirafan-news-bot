085551c5f366d027d364c21fe542cd546a649f00
y52en <y52en@example.com>
2021-03-29 22:09:29 +0900

    add tweet

---
+++ src/tweet.py
@@ -0,0 +1,18 @@
+import os
+import tweepy
+
+# 認証に必要なキーとトークン
+API_KEY = os.environ["twitter_newsbot_api_key"]
+API_SECRET = os.environ["twitter_newsbot_api_secret"]
+ACCESS_TOKEN = os.environ["twitter_newsbot_access_token"]
+ACCESS_TOKEN_SECRET = os.environ["twitter_newsbot_token_secret"]
+
+# APIの認証
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+api.update_status("Hello World")
+
+
+print("end")
